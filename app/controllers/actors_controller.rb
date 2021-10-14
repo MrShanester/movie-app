5d23c2067ae649e8ccb60cabc8ff016995670a52
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-14 15:15:33 -0700

    Fixed incorrect controller directory

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,11 @@
 class ActorsController < ApplicationController
+  def single_actor
+    content = Actor.find_by id: 1
+    render json: content.as_json
+  end
+
+  def all_actors
+    content = Actor.all
+    render json: content.as_json
+  end
 end
