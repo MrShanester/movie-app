35b42ed97d0aa90f3dd3715641c02ae0a4381c35
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-20 14:22:49 -0700

    Adds director and english to update and create actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.new(title: params["title"], year: params["year"], plot: params["plot"])
+    movie = Movie.new(title: params["title"], year: params["year"], plot: params["plot"], director: params["director"], english: params["english"])
     movie.save
     render json: movie.as_json
   end
@@ -29,6 +29,8 @@ class MoviesController < ApplicationController
     movie.title = params["title"] || movie.title
     movie.year = params["year"] || movie.year
     movie.plot = params["plot"] || movie.plot
+    movie.director = params["director"] || movie.director
+    movie.english = params["english"] || movie.english
     movie.save
     render json: movie.as_json
   end
