2a21b6c4b5338216eb7e0c15b40475595f82f2cd
Shane Nicholson <shanescottnicholson@gmail.com>
2021-12-09 09:32:35 -0800

    changes bundler version

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  validates :title, :year, presence: true
-  validates :plot, presence: true, length: {minimum: 25}
+  # validates :title, :year, presence: true
+  # validates :plot, presence: true, length: {minimum: 25}
   has_many :actors
   has_many :movie_genres
   has_many :genres, through: :movie_genres
